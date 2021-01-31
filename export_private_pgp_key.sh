ed5a2578db2f221176955e5e701bc4d2c1c1f707
cx1964 <cx1964@mail.com>
2021-01-31 20:34:45 +0100

    rename scripts

--- export_private_pgp_key.sh
+++ export_private_pgp_key.sh
@@ -1,4 +1,4 @@
 # file: export_private_pgp_key.sh
-# functie: Export de private pgp key vanuit Ubuntu
+# functie: Export de private pgp key vanuit Ubuntu met het command
 #          Laatst getest met ubuntu 20.04 20210130
-gpg --export -a > p.asc
+gpg --export-secret-key -a > private_key.asc
---
+++ export_public_pgp_key.sh
@@ -0,0 +1,4 @@
+# file: export_public_pgp_key.sh
+# functie: Export de public pgp key vanuit Ubuntu
+#          Laatst getest met ubuntu 20.04 20210130
+gpg --export -a > public_key.asc
