3ec650c9b9fd3a7b7c0aeafc15a8065557563dd0
cx1964 <cx1964@mail.com>
2021-01-31 16:50:29 +0100

    ToDo

--- install/install_pgp.sh
+++ install/install_pgp.sh
@@ -2,3 +2,10 @@
 # functie : install gnuPGP packages for ubuntu 20.04
 
 sudo apt-get install gnupg2
+
+# toDo
+1. op oude platform keys exportren
+2. op veilige manier overzetten naar deze machine
+3. keys importeren + dingen doen uit artikel
+   https://makandracards.com/makandra-orga/37763-gpg-extract-private-key-and-import-on-different-machine
+4. python script testen of werkt
