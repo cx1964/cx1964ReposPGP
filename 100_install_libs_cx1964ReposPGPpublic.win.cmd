384760fe92ec733eb25b6ae8e86a22366192e2f0
cx1964 <cx1964@gmail.com>
2021-02-02 08:24:52 +0100

    windows test

---
+++ 100_install_libs_cx1964ReposPGPpublic.win.cmd
@@ -0,0 +1,17 @@
+rem filename: 100_install_libs_cx1964ReposPGPpublic.win.cmd
+rem functie:  install libraries tbv PgP in windows
+
+rem maak virtuele python environment voor python libraries
+python3 -m venv env_python3_pgp
+
+rem activate python env
+.\env_python3_pgp\Scripts\activate.bat
+
+rem upgrade pip
+python3 -m pip install -U pip
+
+rem importeer alle benodigde libraries
+pip install gnupg
+
+rem toon geinstalleerde python libraries
+pip list
